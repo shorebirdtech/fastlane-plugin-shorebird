ad95b8180e44f81525bde939008063c3dd9f5db1
Bryan Oltman <bryan@shorebird.dev>
2025-06-12 17:59:42 -0400

    delete commented-out code

--- lib/fastlane/plugin/shorebird/actions/shorebird_release_action.rb
+++ lib/fastlane/plugin/shorebird/actions/shorebird_release_action.rb
@@ -33,36 +33,6 @@ module Fastlane
            .first
       end
 
-      # def self.generate_export_options_plist(export_options)
-      #   export_options_hash = {}
-      #   if export_options.kind_of?(Hash)
-      #     export_options_hash = export_options
-      #     export_options_hash[:method] = "app-store"
-      #     provisioning_profile_mapping = Fastlane::Actions.lane_context[SharedValues::MATCH_PROVISIONING_PROFILE_MAPPING]
-      #     if provisioning_profile_mapping
-      #       # If match has provided provisioning profiles, put them in the export options plist
-      #       export_options_hash[:provisioningProfiles] = provisioning_profile_mapping
-      #       export_options_hash[:signingStyle] = 'manual'
-      #     end
-      #   elsif export_options.kind_of?(String)
-      #     export_options_path = File.expand_path(export_options)
-      #     unless File.exist?(export_options_path)
-      #       raise "export_options path #{export_options_path} does not exist"
-      #     end
-
-      #     export_options_hash = Plist.parse_xml(export_options_path)
-      #   end
-
-      #   # If manageAppVersionAndBuildNumber is not false, Shorebird won't
-      #   # work. If set to true (or not provided), Xcode will change the build
-      #   # number *after* the release is created, causing the app to be
-      #   # unpatchable.
-      #   export_options_hash[:manageAppVersionAndBuildNumber] = false
-      #   export_options_hash.compact!
-
-      #   ExportOptionsPlistGenerator.generate(export_options_hash)
-      # end
-
       def self.description
         "Create a Shorebird release"
       end
